ca39a9f1be61490a01efa1ebf08cd5e1255dac76
PitiasF <selamptsegu@gmail.com>
2020-09-03 16:00:08 -0400

    adding more API Endpoint

---
+++ RestAPI/src/test/java/apiScript/Library.java
@@ -0,0 +1,61 @@
+package apiScript;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.List;
+
+import com.jayway.restassured.response.Header;
+
+public class Library {
+
+	public List<Header> getHeader(String url, String body, String email, int statue, String content) {
+
+		List<Header> header =
+
+				given().when().get(url).then().assertThat().statusCode(statue).body(body, equalTo(email))
+						.body("data[2].avatar", notNullValue()).header("content-type", equalTo(content)).extract()
+						.headers().asList();
+
+		return header;
+	}
+
+	public String getContent(String url, int statues, String body, String email, String contents) {
+
+		String content =
+
+				given().when().get(url).then().assertThat().statusCode(statues).body(body, equalTo(email))
+						.body("data[2].avatar", notNullValue()).header("content-type", equalTo(contents)).extract()
+						.header("content-type");
+
+		return content;
+
+	}
+
+	public String getData(String url, int id, int statue, String body, String email) {
+
+		String data = given().param("id", id).when().get(url).then().assertThat().statusCode(statue)
+				.body(body, equalTo(email)).extract().body().asString();
+
+		return data;
+	}
+
+	public String getString(String User, int statue, String body, String name, String json) {
+		String data =
+
+				given().param("id", 3).when().get(User).then().assertThat().statusCode(statue)
+						.body("data.avatar", notNullValue()).body(body, equalTo(name)).extract().body().jsonPath()
+						.get(json);
+
+		return data;
+	}
+	public void post(String body,String url,int statue,String ctype) {
+		
+		given().body(body).when().post(url).then().assertThat().statusCode(statue)
+		.header("content-type", equalTo(ctype)).body("id", notNullValue())
+		.body("createdAt", notNullValue());
+	
+	}
+
+}
---
+++ RestAPI/src/test/java/apiScript/RestAPI.java
@@ -0,0 +1,127 @@
+package apiScript;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.response.Header;
+
+public class RestAPI {
+
+	String BaseURL = "https://reqres.in/api/";
+	String User = BaseURL + "users";
+	String Register = BaseURL + "users/register";
+	String Login = BaseURL + "users/login";
+	Library lib;
+	String data3;
+	String text;
+
+
+	@Test(enabled = true)
+	public void getHeader() {
+		// Ctrl + Space
+		lib = new Library();
+
+		List<Header> header = lib.getHeader(User, "data[2].email", "emma.wong@reqres.in", 200,
+				"application/json; charset=utf-8");
+
+		Iterator<Header> itr = header.iterator();
+		while (itr.hasNext()) {
+			Header data = itr.next();
+			System.out.println(data);
+
+		}
+
+	}
+
+	@Test(enabled = true)
+
+	public void getContentType() {
+		
+
+		String data = given().when().get(User).then().assertThat().statusCode(200)
+				.body("data[2].email", equalTo("emma.wong@reqres.in")).body("data[2].avatar", notNullValue())
+				.header("content-type", equalTo("application/json; charset=utf-8")).extract().header("content-type");
+
+		System.out.println("The Content type is " + data);
+
+	}
+
+	@Test(enabled = true)
+	public void getThirdData() {
+		// Get the data and validate the email
+
+		try {
+			data3 = lib.getData(User, 3, 200, "data.email", "emma.wong@reqres.in");
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+
+
+		System.out.println(data3);
+	}
+
+	@Test(enabled = true)
+	public void getEmmaText() {
+		try {
+			text = lib.getString(User, 200, "data.first_name", "Emma", "ad.text");
+
+		} catch (Exception e)
+
+		{
+			System.out.println(e.getMessage());
+		}
+
+
+
+		System.out.println("Emma Text: " + text);
+
+	}
+
+	@Test(enabled = true)
+	public void postData() {
+
+		String body = "{'name':'me','job':'qa'}"; // Data
+		try {
+			lib.post(body, User, 201, "application/json; charset=utf-8");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+
+
+	}
+
+	@Test(enabled = true)
+	public void extractAndLoginTest() {
+
+		// Extract a Person Register,SignIn and Verify
+
+		String body = "{'email': 'sydney@fife','password': 'pistol'}";
+		String body1 = "{'email':'peter@kleven','password': 'cityslika'}";
+
+		// or
+		String email = given().param("id", 3).when().get(User).then().assertThat().statusCode(200)
+				.body("data.email", equalTo("emma.wong@reqres.in")).extract().body().jsonPath().getString("data.email");
+
+		String LoginBody = "{'email':'" + email + "','password':'bond'}"; // Caution ???
+		String registerBody = "{'email':'" + email + "'}";
+
+		given().body(registerBody).when().post(Register).then().assertThat().statusCode(201);
+
+		given().body(LoginBody).when().post(Login).then().assertThat().statusCode(201);
+
+		
+		// Refactoring Code Next ???
+	
+
+	}
+
+}
